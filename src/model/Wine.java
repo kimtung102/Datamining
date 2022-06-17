ddc7bf1c34809a4ec577de76be17a0c0bc3eac3c
luantran2k <tranluan2kyl@gmail.com>
2022-06-17 21:55:50 +0700

    refactor Wine

--- src/model/Wine.java
+++ src/model/Wine.java
@@ -29,7 +29,9 @@ public class Wine {
     private float sulphates;
     private float alcohol;
 
-    public Wine(float fixedAcidity, float volatileAcidity, float citricAcid, float residualSugar, float chlorides, float freeSulfurDioxide, float totalSulfurDioxide, float density, float pH, float sulphates, float alcohol) {
+    public Wine(float fixedAcidity, float volatileAcidity, float citricAcid, float residualSugar,
+            float chlorides, float freeSulfurDioxide, float totalSulfurDioxide,
+            float density, float pH, float sulphates, float alcohol) {
         this.fixedAcidity = fixedAcidity;
         this.volatileAcidity = volatileAcidity;
         this.citricAcid = citricAcid;
@@ -41,71 +43,6 @@ public class Wine {
         this.pH = pH;
         this.sulphates = sulphates;
         this.alcohol = alcohol;
-    }
-
-    public Wine() {
-
-    }
-
-    public void dataToArff() throws Exception {
-        FastVector atts;
-        FastVector attVals;
-        Instances data;
-        double[] vals;
-        int i;
-
-        //setup attribute
-        atts = new FastVector();
-        //numberic
-        atts.addElement(new Attribute("fixed acidity"));
-        atts.addElement(new Attribute("volatile acidity"));
-        atts.addElement(new Attribute("citric acid"));
-        atts.addElement(new Attribute("residual sugar"));
-        atts.addElement(new Attribute("chlorides"));
-        atts.addElement(new Attribute("free sulfur dioxide"));
-        atts.addElement(new Attribute("total sulfur dioxide"));
-        atts.addElement(new Attribute("density"));
-        atts.addElement(new Attribute("pH"));
-        atts.addElement(new Attribute("sulphates"));
-        atts.addElement(new Attribute("alcohol"));
-        //nominal
-        attVals = new FastVector();
-        for (i = 3; i <= 8; i++) {
-            attVals.addElement(String.valueOf(i));
-        }
-        atts.addElement(new Attribute("quality", attVals));
-        
-        // create Instances object
-        data = new Instances("test", atts, 0);
-        // fill data
-        // my instance
-        vals = new double[data.numAttributes()];
-        // - numeric
-        vals[0] = fixedAcidity;
-        vals[1] = volatileAcidity;
-        vals[2] = citricAcid;
-        vals[3] = residualSugar;
-        vals[4] = chlorides;
-        vals[5] = freeSulfurDioxide;
-        vals[6] = totalSulfurDioxide;
-        vals[7] = density;
-        vals[8] = pH;
-        vals[9] = sulphates;
-        vals[10] = alcohol;
-        //missing value for predict
-        vals[11] = Utils.missingValue();
-        //add
-        data.add(new DenseInstance(1.0, vals));
-        System.out.println(data);
-        
-        //xuat data ra file arff
-//        BufferedWriter outWriter = new BufferedWriter(new FileWriter(currentPath + "\\data\\wine-unlabel.arff"));
-//        outWriter.write(data.toString());
-//        outWriter.newLine();
-//        outWriter.flush();
-//        outWriter.close();
-        
-        
     }
 
     public Instance getInstanceData() {
